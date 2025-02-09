574fcbdb18a5d93193d58b0bafe9bc6acd61576b
Jiancheng Zheng <zhjc29@126.com>
2025-02-09 13:11:41 +0800

    test-4

---
+++ test/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  test
+//
+//  Created by 郑建城 on 2025/2/9.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+#Preview {
+    test()
+    //print("hello")
+}
