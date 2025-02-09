171ab282ff24bf5d8ed67a5405f513cd8d369079
Jiancheng Zheng <zhjc29@126.com>
2025-02-09 09:56:54 +0800

    test-3

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world-3!")
+            Text("Hello, world war 3!")
         }
         .padding()
         .padding()
